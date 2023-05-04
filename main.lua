1eeb749f0771730d7bb76259248493ec546b08a8
TimotheGonin <gonin.timothe@gmail.com>
2023-05-04 15:35:57 +0200

    display lander + angle

--- main.lua
+++ main.lua
@@ -1,11 +1,25 @@
 -- PRINTER
 io.stdout:setvbuf('no')
 
+local Lander = {}
+Lander.x = 0
+Lander.Y = 0
+Lander.angle = -90
+Lander.velocity_x = 0
+Lander.velocity_y = 0
+Lander.image = love.graphics.newImage('images/ship.png')
+
 function love.load()
+  screen_width = love.graphics.getWidth()
+  screen_height = love.graphics.getHeight()
+
+  Lander.x = screen_width/2
+  Lander.y = screen_height/2
 end
 
 function love.update(dt)
 end
 
 function love.draw()
+  love.graphics.draw(Lander.image, Lander.x, Lander.y, math.rad(Lander.angle), 1, 1, Lander.image:getWidth()/2, Lander.image:getHeight()/2)
 end
