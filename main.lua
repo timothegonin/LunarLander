f98f0782ec9d1f4cd49b80cb7b7b273eb0bfb77c
TimotheGonin <gonin.timothe@gmail.com>
2023-05-05 15:02:06 +0200

    engine on animation

--- main.lua
+++ main.lua
@@ -8,6 +8,8 @@ Lander.angle = -90
 Lander.velocity_x = 0
 Lander.velocity_y = 0
 Lander.image = love.graphics.newImage('images/ship.png')
+Lander.image_engine = love.graphics.newImage('images/engine.png')
+Lander.engineOn = false
 
 function love.load()
   screen_width = love.graphics.getWidth()
@@ -27,7 +29,11 @@ function love.update(dt)
   if love.keyboard.isDown("left") then
     Lander.angle = Lander.angle - (90 * dt)
   end
+  --engine on
   if love.keyboard.isDown("up") then
+    Lander.engineOn = true
+  else 
+    Lander.engineOn = false
   end
 
   Lander.x = Lander.x + Lander.velocity_x
@@ -36,4 +42,8 @@ end
 
 function love.draw()
   love.graphics.draw(Lander.image, Lander.x, Lander.y, math.rad(Lander.angle), 1, 1, Lander.image:getWidth()/2, Lander.image:getHeight()/2)
+  
+  if Lander.engineOn == true then 
+    love.graphics.draw(Lander.image_engine, Lander.x, Lander.y, math.rad(Lander.angle), 1, 1, Lander.image_engine:getWidth()/2, Lander.image_engine:getHeight()/2)
+  end
 end
